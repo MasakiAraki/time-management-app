010bf8e4b7dfb924c4f038eda928af24598e039f
MasakiAraki <kgmsk411@gmail.com>
2025-01-28 22:39:25 +0900

    feat: add environment-specific database configuration for development, test, and production

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,9 +1,25 @@
 require('dotenv').config()
 
 module.exports = {
-  username: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DB,
-  host: process.env.DATABASE_HOST,
-  dialect: 'postgres'
+  development: {
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    host: process.env.DATABASE_HOST,
+    dialect: 'postgres'
+  },
+  test: {
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    host: process.env.DATABASE_HOST,
+    dialect: 'postgres'
+  },
+  production: {
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    host: process.env.DATABASE_HOST,
+    dialect: 'postgres'
+  }
 }
